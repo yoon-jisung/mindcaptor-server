79ab3bab09c58c0d39b285c9db667598ab60c584
jeasong <cjs5241@gmail.com>
2021-04-29 17:05:27 +0900

    [server]/#5/feat:database model done

--- config/config.js
+++ config/config.js
@@ -12,13 +12,6 @@ module.exports = {
     dialect: 'mysql',
     logging: false
   },
-  test: {
-    username: 'root',
-    password: process.env.DATABASE_PASSWORD,
-    database: 'mindcaptorData',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
   production: {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
