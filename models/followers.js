d3472ea6d36b143f6c624d8ad76fc07dd35546db
Youjin Lee <youjinlee1997@gmail.com>
2021-04-30 02:19:16 +0900

    [server] / #41 / fix: edit database model

--- models/followers.js
+++ models/followers.js
@@ -1,23 +1,27 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-
-  class followers extends Model {
-
-    static associate(models) {
-      followers.belongsTo(models.users, {foreignKey: 'following_id', targetKey: 'id'});
-      followings.belongsTo(models.users, {foreignKey: 'followed_id', targetKey: 'id'});
+    class followers extends Model {
+        static associate(models) {
+            followers.belongsTo(models.users, {
+                foreignKey: 'following_id',
+                targetKey: 'id',
+            });
+            followers.belongsTo(models.users, {
+                foreignKey: 'followed_id',
+                targetKey: 'id',
+            });
+        }
     }
-  }
-  followers.init(
-    {
-        following_id: DataTypes.INTEGER,
-        followed_id: DataTypes.INTEGER,
-      },
-    {
-      sequelize,
-      modelName: 'followers',
-    }
-  );
-  return followers;
+    followers.init(
+        {
+            following_id: DataTypes.INTEGER,
+            followed_id: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: 'followers',
+        }
+    );
+    return followers;
 };
