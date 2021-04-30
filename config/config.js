dcbb888df6d9a6be48d5b5ca7fb15ebd11e896b1
Youjin Lee <youjinlee1997@gmail.com>
2021-04-30 13:26:05 +0900

    [server] / #44 / conflict resolve

--- config/config.js
+++ config/config.js
@@ -1,23 +1,29 @@
-
 const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  development: {
-    username: process.env.DATABASE_USER,//'root',
-    password: process.env.DATABASE_PASSWORD,
-    database: 'mindcaptorData',
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: 'mysql',
-    logging: false
-  },
-  production: {
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: 'midcaptorData',
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: 'mysql',
-  },
+    development: {
+        username: process.env.LOCAL_USERNAME,
+        password: process.env.LOCAL_PASSWORD,
+        database: process.env.LOCAL_DB_NAME,
+        host: process.env.LOCAL_HOST,
+        dialect: 'mysql',
+        logging: false,
+    },
+    test: {
+        username: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.TEST_DB_NAME,
+        host: process.env.RDS_HOST,
+        port: process.env.RDS_PORT,
+        dialect: 'mysql',
+    },
+    production: {
+        username: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.PRODUCTION_DB_NAME,
+        host: process.env.RDS_HOST,
+        port: process.env.RDS_PORT,
+        dialect: 'mysql',
+    },
 };
