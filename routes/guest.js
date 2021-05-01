6c11d818c6d632687d1ae2c09ef09d75dd2c3665
Youjin Lee <moo.moo.moojin@gmail.com>
2021-05-01 12:43:00 +0900

    Revert "Feature #05 database setting"

--- routes/guest.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/guest.js');
-
-router.post('/', (req, res, next) => {
-    // 컨트롤러 함수 호출
-});
-
-module.exports = router;
