c2a4b13b122077a1343ef13f687d3a5dc5fd8476
Youjin Lee <youjinlee1997@gmail.com>
2021-05-06 10:37:53 +0900

    [server] / #15 / fix: joinRoom 수정

--- controllers/room.js
+++ controllers/room.js
@@ -2,12 +2,18 @@ const User = require('../models/index.js').users;
 const Room = require('../models/index.js').rooms;
 const Guest = require('../models/index.js').guests;
 const socketConnection = require('./socketConnection');
+const jwt = require('jsonwebtoken');
 
 const joinUser = async (req, room) => {
     // authorization 헤더 내용물이 `Bearer {Token}` 형식이기 때문에 가공해주어야 한다.
-    const splited = authorization.split(' ')[1];
-    const userData = jwt.verify(splited, process.env.ACCESS_SECRET);
-    await User.update({ room_id: room.id }, { where: { id: userData.userId } });
+    const userData = jwt.verify(
+        req.headers.authorization,
+        process.env.ACCESS_SECRET
+    );
+    await User.update(
+        { room_id: room.dataValues.id },
+        { where: { id: userData.id } }
+    );
 };
 
 module.exports = {
@@ -16,6 +22,7 @@ module.exports = {
     },
     join: async (req, res) => {
         // accessToken이 없으면 반려
+
         if (!req.headers.authorization) {
             res.status(401).send('accessToken이 없습니다.');
         } else if ((await Room.count()) === 0) {
@@ -24,7 +31,6 @@ module.exports = {
             let room;
             while (1) {
                 // 1.방 하나를 가져온다
-                await Room.create({ room_name: 1, id: 1 }); // 테스트용 방 생성
                 room = await Room.findOne();
 
                 // 2. 해당 방과 연결된 User와 Guest들을 찾는다
@@ -34,12 +40,15 @@ module.exports = {
                 });
                 // 3-1. 유저들이 4명 이하이면 사용자를 해당 방에 입장시킨다
                 if (userNum + guestNum < 4) {
-                    await joinUser(req);
+                    await joinUser(req, room);
                     break;
                 }
                 // 3-2. 유저들이 4명 이상이면 다시 1번으로 돌아간다
             }
-            res.redirect('../room/' + room.id); // 방에 입장
+            res.status(200).json({
+                data: room.dataValues.id,
+                message: 'joinRooom',
+            });
         }
     },
     new: async (req, res) => {
@@ -54,8 +63,10 @@ module.exports = {
                 answer: null,
             });
             // await joinUser(req, newRoom); // 2. 사용자를 그 방과 연결시킨다.
-            res.status(201).json({data:newRoom.dataValues.id,message:'createdRooom'})
-            //res.redirect('../' + newRoom.id);
+            res.status(201).json({
+                data: newRoom.dataValues.id,
+                message: 'createdRooom',
+            });
         }
     },
 };
